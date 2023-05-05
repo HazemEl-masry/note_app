ff0d8dd0e85225b2b78e9b97a863dcf47ef84133
HazemEl-masry <hazemelmasryofficial@gmail.com>
2023-05-06 02:46:41 +0300

    bottom sheet shape

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class AddNotaBottomSheet extends StatelessWidget {
   const AddNotaBottomSheet({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 32.0,
+        right: 16.0,
+        left: 16.0
+      ),
+      child: Column(
+        children: const [
+          CustomTextField()
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_text_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16.0),
+        )
+      ),
+    );
+  }
+}
